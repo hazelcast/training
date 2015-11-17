2a76f3e657513b4a2c6890d6c6f058e77f93e41b
Viktor Gamov <gamussa@gmail.com>
2015-11-17 13:10:35 -0500

    updated mapstore, added Lifecycle support example

--- caching/src/main/java/com/hazelcast/training/caching/lab3/jdbc/CompanyMapStore.java
+++ caching/src/main/java/com/hazelcast/training/caching/lab3/jdbc/CompanyMapStore.java
@@ -1,41 +1,29 @@
 package com.hazelcast.training.caching.lab3.jdbc;
 
 import com.google.common.collect.Sets;
+import com.hazelcast.core.HazelcastInstance;
+import com.hazelcast.core.MapLoaderLifecycleSupport;
 import com.hazelcast.core.MapStore;
 import com.hazelcast.training.caching.dto.Company;
 
 import java.sql.*;
-import java.util.Collection;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
 
 import static java.lang.String.format;
 
-public class CompanyMapStore implements MapStore<Integer, Company> {
+public class CompanyMapStore implements MapStore<Integer, Company>, MapLoaderLifecycleSupport {
 
-    private final Connection con;
+    private Connection con;
     private PreparedStatement allKeysStatement;
 
     public CompanyMapStore() {
-        try {
-            //
-            //con = DriverManager.getConnection("jdbc:hsqldb:file:/tmp/hazelcastdb", "SA", "");
-
-            con = DriverManager.getConnection("jdbc:hsqldb:hsql://localhost:9002/hazelcastdb", "sa", "");
-            con.createStatement().executeUpdate(
-                    "create table if not exists company (id int not null, name varchar(45), primary key (id))");
-            allKeysStatement = con.prepareStatement("select id from company");
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        }
     }
 
     public synchronized void delete(Integer key) {
         System.out.println("Delete:" + key);
         try {
             con.createStatement().executeUpdate(
-                    format("delete from company where id = %s", key));
+                format("delete from company where id = %s", key));
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
@@ -44,7 +32,7 @@ public class CompanyMapStore implements MapStore<Integer, Company> {
     public synchronized void store(Integer key, Company value) {
         try {
             con.createStatement().executeUpdate(
-                    format("insert into company values(%s,'%s')", key, value.getCompanyName()));
+                format("insert into company values(%s,'%s')", key, value.getCompanyName()));
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
@@ -56,15 +44,17 @@ public class CompanyMapStore implements MapStore<Integer, Company> {
     }
 
     public synchronized void deleteAll(Collection<Integer> keys) {
-        for (Integer key : keys) delete(key);
+        for (Integer key : keys)
+            delete(key);
     }
 
     public synchronized Company load(Integer key) {
         try {
             ResultSet resultSet = con.createStatement().executeQuery(
-                    format("select name from company where id =%s", key));
+                format("select name from company where id =%s", key));
             try {
-                if (!resultSet.next()) return null;
+                if (!resultSet.next())
+                    return null;
                 String name = resultSet.getString(1);
                 return new Company(key, name);
             } finally {
@@ -77,7 +67,8 @@ public class CompanyMapStore implements MapStore<Integer, Company> {
 
     public synchronized Map<Integer, Company> loadAll(Collection<Integer> keys) {
         Map<Integer, Company> result = new HashMap<Integer, Company>();
-        for (Integer key : keys) result.put(key, load(key));
+        for (Integer key : keys)
+            result.put(key, load(key));
         return result;
     }
 
@@ -86,4 +77,28 @@ public class CompanyMapStore implements MapStore<Integer, Company> {
         return Sets.newHashSet(keys);
     }
 
+    @Override
+    public void init(HazelcastInstance hazelcastInstance, Properties properties, String mapName) {
+        try {
+            //
+            //con = DriverManager.getConnection("jdbc:hsqldb:file:/tmp/hazelcastdb", "SA", "");
+
+            con = DriverManager.getConnection((String) properties.get("dburl"), "sa", "");
+            con.createStatement().executeUpdate(
+                "create table if not exists company (id int not null, name varchar(45), primary key (id))");
+            allKeysStatement = con.prepareStatement("select id from company");
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+
+    @Override public void destroy() {
+        try {
+            con.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+    }
 }
