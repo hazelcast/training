62765705a0819a99d9ca6b0c1b7ff618418f6864
Neil Stevenson <neil.stevenson@gmail.com>
2016-10-11 09:15:31 +0100

    Note minor issue if no networking

--- essentials/labs/lab1/src/main/java/XMLMain.java
+++ essentials/labs/lab1/src/main/java/XMLMain.java
@@ -2,9 +2,11 @@ import com.hazelcast.client.HazelcastClient;
 import com.hazelcast.core.Hazelcast;
 import com.hazelcast.core.HazelcastInstance;
 
+// https://github.com/hazelcast/hazelcast/issues/9081
+
 public class XMLMain {
     public static void main(String[] args) {
-        HazelcastInstance hz = Hazelcast.newHazelcastInstance();
+    	HazelcastInstance hz = Hazelcast.newHazelcastInstance();
         HazelcastInstance client = HazelcastClient.newHazelcastClient();
     }
 }
