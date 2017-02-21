acf6df153ac7c714cdf8550e575632c7afa6747c
Neil Stevenson <neilstevenson@users.noreply.github.com>
2017-02-21 10:57:29 +0000

    Upgrade to 3.8

--- essentials/lab1/src/main/java/XMLMain.java
+++ essentials/lab1/src/main/java/XMLMain.java
@@ -2,8 +2,6 @@ import com.hazelcast.client.HazelcastClient;
 import com.hazelcast.core.Hazelcast;
 import com.hazelcast.core.HazelcastInstance;
 
-// https://github.com/hazelcast/hazelcast/issues/9081
-
 public class XMLMain {
     public static void main(String[] args) {
     	HazelcastInstance hz = Hazelcast.newHazelcastInstance();
