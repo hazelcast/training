18e30ce72a2e68f4fc7fa911143c5f58943b9495
Neil Stevenson <neilstevenson@users.noreply.github.com>
2017-10-24 14:25:50 +0100

    Fix for Spring 5

--- bootcamp/labs/lab3/lab3-server/src/main/java/com/hazelcast/techops/training/bootcamp/HazelcastSpringConfiguration.java
+++ bootcamp/labs/lab3/lab3-server/src/main/java/com/hazelcast/techops/training/bootcamp/HazelcastSpringConfiguration.java
@@ -2,7 +2,6 @@ package com.hazelcast.techops.training.bootcamp;
 
 import java.util.Arrays;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
