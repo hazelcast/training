cc9ff0f99ba1c30911097c52111ee8f721b84dca
Rahul Gupta <rahul@hazelcast.com>
2018-05-22 09:51:49 +1000

    3.10 refactoring

--- advanced/lab1/src/main/java/com/hazelcast/Main.java
+++
@@ -1,9 +0,0 @@
-package com.hazelcast;
-
-public class Main {
-    public static void main(String[] args) {
-        // Add necessary code to start up a Hazelcast instance and
-        // run the application multiple times to cluster them up
-
-    }
-}
